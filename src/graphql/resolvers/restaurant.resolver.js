60f31557ce8b0ad2928ac32e8db72fa914996715
Oscar Alberto Sanchez Santamaria <oasanchezs@falabella.com.co>
2022-08-03 14:26:20 -0500

    mod: Se agrega un toString para recibir el ID de mongo como un string y poder compararlo correctamente

--- src/graphql/resolvers/restaurant.resolver.js
+++ src/graphql/resolvers/restaurant.resolver.js
@@ -3,22 +3,21 @@ const restaurantModel = require("../../models/restaurant.model");
 // The root provides a resolver function for each API endpoint
 var root = {
     restaurant: async (args) => {
-        var id = args.id;
+        const _id = args.id;
         const restaurantData = await restaurantModel.find({});
         return restaurantData.filter(restaurant => {
-            return restaurant.id == id;
+            return restaurant._id.toString() === _id;
         })[0];
     },
     restaurants: async (args) => {
         const restaurantData = await restaurantModel.find({});
-        var result = restaurantData;
+        const result = restaurantData;
         if (args.type) {
             result = result.filter(restaurant => restaurant.type === args.type);
         }
         if (args.stars) {
             result = result.filter(restaurant => restaurant.stars === args.stars);
         }
-
         return result;
     }
 };
